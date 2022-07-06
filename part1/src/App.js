b26e38883acf9f72d803522aae3ea598ade3ba8b
biko2020 <brahimlion38@hotmail.com>
2022-07-06 13:46:01 +0100

    create multiple components

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,7 +1,29 @@
-const App = () => {
+const Hello = (props) => {
+return (
   <div>
-    <p>Hello world</p>
+    <p>Hello {props.name}, you are {props.age}</p>
   </div>
+)
 }
 
-export default App
+const Footer = () => {
+  return(
+    <div>
+      greeting app created by <a href="http://github.com/mluukkai">mluukkai</a>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peters'
+  const age = 10
+  return (
+    <div>
+      <h1>Geentings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+      <Footer />
+    </div>
+  );
+};
+export default App;
