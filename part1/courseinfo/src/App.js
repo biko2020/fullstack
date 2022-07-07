8be1745565487f787651e2a8b5938d26637726f9
biko2020 <brahimlion38@hotmail.com>
2022-07-07 08:50:50 +0100

    step2 Three Part components

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -7,55 +7,76 @@ const Header = (props) => {
 };
 
 const Content = () => {
-  
   return (
     <div>
-      
-       <Part />
-     
+      <PartOne />
+      <PartTwo />
+      <PartThree />
     </div>
   );
 };
 
-const Part = () => {
-  const part1 = 'Fundamentals of React';
+const PartOne = () => {
+  const part1 = "Fundamentals of React";
   const exercises1 = 10;
-  const part2 = 'Using props to pass data';
+
+  return (
+    <div>
+      <p>
+        {part1} {exercises1}
+      </p>
+    </div>
+  );
+};
+
+const PartTwo = () => {
+  const part2 = "Using props to pass data";
   const exercises2 = 7;
-  const part3 = 'State of a component';
+
+  return (
+    <div>
+      <p>
+        {part2} {exercises2}
+      </p>
+    </div>
+  );
+};
+
+const PartThree = () => {
+  const part3 = "State of a component";
   const exercises3 = 14;
 
-  return(
+  return (
     <div>
-    <p>{part1} {exercises1}</p>
-    <p>{part2} {exercises2}</p>
-    <p>{part3} {exercises3}</p>
-
-  </div>
-  )
-
-}
+      <p>
+        {part3} {exercises3}
+      </p>
+    </div>
+  );
+};
 
 const Total = (props) => {
-return(
-  <div>
-    <p>{props.msg} is {props.total}</p>
-  </div>
-)
-}
+  return (
+    <div>
+      <p>
+        {props.msg} is {props.total}
+      </p>
+    </div>
+  );
+};
 
 const App = () => {
   const course = "Half Stack application development";
   const exercises1 = 10;
   const exercises2 = 7;
   const exercises3 = 14;
-  const msg = 'total number of exercises'
+  const msg = "total number of exercises";
 
   return (
     <div>
       <Header course={course} />
       <Content />
-      <Total  msg={msg}  total={exercises1 + exercises2 +exercises3}/>
+      <Total msg={msg} total={exercises1 + exercises2 + exercises3} />
     </div>
   );
 };
